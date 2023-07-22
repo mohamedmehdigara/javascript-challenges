48a0f2d2446cceaf00d261b9eda5974fac129587
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-22 19:12:25 +0100

    added file

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,15 @@
-const addEventing = function (obj) {
+
+const myObj = { name: 'John', age: 35 };
+
+const addEventing = function(myObj){
+
+
+
+myObj.on("ageChange", function () {
+  console.log("age changed");
+});
+
+myObj.trigger("ageChange"); // logs 'age changed'
 }
 
 module.exports = addEventing
