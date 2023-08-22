7b3c9c58af6c9f0ce81a1e9cac6aad07e8a972cb
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-22 15:20:38 +0100

    edited files

--- babylonian-method/babylonian.js
+++ babylonian-method/babylonian.js
@@ -1,5 +1,17 @@
 const squareRoot = (radicand) => {
-  return radicand
+  let guess = radicand / 2;
+
+  // Define a tolerance level for approximation
+  const tolerance = 1e-6; // You can adjust the tolerance level
+
+  // Iterate until the difference between guess and actual answer is within tolerance
+  while (Math.abs(guess * guess - radicand) > tolerance) {
+    // Step 2: Calculate a new guess using the Babylonian method
+    guess = (guess + radicand / guess) / 2;
+  }
+
+  // Return the final approximation
+  return guess;
 }
 
 module.exports = squareRoot
