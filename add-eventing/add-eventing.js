77f783c33258fbb3f2bfd013d6fb0a3a8fa1b6ea
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-22 17:05:56 +0100

    edited files

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,15 +1,36 @@
+const addEventing = function (obj) {
+  // Create an object to store event listeners
+  const events = {};
 
-const myObj = { name: 'John', age: 35 };
+  // Add the 'on' property to register event listeners
+  obj.on = function (eventName, callback) {
+    if (!events[eventName]) {
+      events[eventName] = [];
+    }
+    events[eventName].push(callback);
+  };
 
-const addEventing = function(myObj){
+  // Add the 'trigger' property to execute all event listeners for the given event name
+  obj.trigger = function (eventName, ...args) {
+    if (events[eventName]) {
+      events[eventName].forEach(callback => {
+        callback(...args);
+      });
+    }
+  };
 
+  // Return the object with the added properties 'on' and 'trigger'
+  return obj;
+};
 
+const obj = addEventing({ name: 'Kelsey', age: 30 });
 
-myObj.on("ageChange", function () {
+// Now, you can use the 'on' and 'trigger' properties of the obj
+obj.on("ageChange", function () {
   console.log("age changed");
 });
 
-myObj.trigger("ageChange"); // logs 'age changed'
-}
+obj.trigger("ageChange"); // logs 'age changed'
 
-module.exports = addEventing
+module.exports = addEventing;
+
