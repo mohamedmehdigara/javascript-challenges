fa7ab4c27d294a4d315c0de447e8982c09506a7e
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-22 17:43:23 +0100

    edited files

--- balanced-parens/balanced-parens-test.js
+++ balanced-parens/balanced-parens-test.js
@@ -1,39 +1,39 @@
-const test = require('tape')
-const parensAreBalanced = require('./balanced-parens')
+const test = require('tape');
+const parensAreBalanced = require('./balanced-parens');
 
 test('it matches braces when only one pair of one type exists', assert => {
-  assert.equal(parensAreBalanced('()'), true)
-  assert.equal(parensAreBalanced('[]'), true)
-  assert.equal(parensAreBalanced('('), false)
-  assert.equal(parensAreBalanced(']'), false)
+  assert.equal(parensAreBalanced('()'), true);
+  assert.equal(parensAreBalanced('[]'), true);
+  assert.equal(parensAreBalanced('('), false);
+  assert.equal(parensAreBalanced(']'), false);
 
-  assert.end()
-})
+  assert.end();
+});
 
-test.skip('it matches braces when multiple pairs of one type exist', assert => {
-  assert.equal(parensAreBalanced('((()))'), true)
-  assert.equal(parensAreBalanced('((())'), false)
+test('it matches braces when multiple pairs of one type exist', assert => {
+  assert.equal(parensAreBalanced('((()))'), true);
+  assert.equal(parensAreBalanced('((())'), false);
 
-  assert.end()
-})
+  assert.end();
+});
 
-test.skip('it matches braces when multiple pairs of multiple types exist', assert => {
-  assert.equal(parensAreBalanced('{([])}'), true)
-  assert.equal(parensAreBalanced('{([]}'), false)
+test('it matches braces when multiple pairs of multiple types exist', assert => {
+  assert.equal(parensAreBalanced('{([])}'), true);
+  assert.equal(parensAreBalanced('{([]}'), false);
 
-  assert.end()
-})
+  assert.end();
+});
 
-test.skip('it works correctly when no braces exist', assert => {
-  assert.equal(parensAreBalanced(''), true)
-  assert.equal(parensAreBalanced('hello world'), true)
+test('it works correctly when no braces exist', assert => {
+  assert.equal(parensAreBalanced(''), true);
+  assert.equal(parensAreBalanced('hello world'), true);
 
-  assert.end()
-})
+  assert.end();
+});
 
-test.skip('it matches braces when braces and other characters exist', assert => {
-  assert.equal(parensAreBalanced('function () { console.log(["cat", "dog"]) }'), true)
-  assert.equal(parensAreBalanced('function ( { console.log(["cat", "dog"]) }'), false)
+test('it matches braces when braces and other characters exist', assert => {
+  assert.equal(parensAreBalanced('function () { console.log(["cat", "dog"]) }'), true);
+  assert.equal(parensAreBalanced('function ( { console.log(["cat", "dog"]) }'), false);
 
-  assert.end()
-})
+  assert.end();
+});
--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,20 @@
 const parensAreBalanced = (input) => {
-  return false
+  const stack = [];
+  const openingParentheses = "([{";
+  const closingParentheses = ")]}";
+
+  for (const char of input) {
+    if (openingParentheses.includes(char)) {
+      stack.push(char);
+    } else if (closingParentheses.includes(char)) {
+      const lastOpened = stack.pop();
+      if (!lastOpened || openingParentheses.indexOf(lastOpened) !== closingParentheses.indexOf(char)) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
 
-module.exports = parensAreBalanced
+module.exports = parensAreBalanced;
