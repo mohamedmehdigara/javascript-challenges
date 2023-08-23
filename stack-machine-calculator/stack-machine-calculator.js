e8cd58a8fc302ca2e20e81c949162b14d0b0a0f6
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-23 14:17:23 +0100

    edited files

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,26 @@
-const stackMachineCalculator = (instructions) => {
-  return instructions
+function stackMachineCalculator(instructions) {
+  const stack = [];
+  const instrTokens = instructions.split(' ');
+
+  for (const token of instrTokens) {
+    if (token === 'ADD') {
+      const b = stack.pop();
+      const a = stack.pop();
+      stack.push(a + b);
+    } else if (token === 'SUB') {
+      const b = stack.pop();
+      const a = stack.pop();
+      stack.push(a - b);
+    } else if (token === 'POP') {
+      stack.pop();
+    } else if (token === 'DUP') {
+      stack.push(stack[stack.length - 1]);
+    } else {
+      stack.push(parseInt(token));
+    }
+  }
+
+  return stack[stack.length - 1];
 }
 
-module.exports = stackMachineCalculator
+module.exports = stackMachineCalculator;
