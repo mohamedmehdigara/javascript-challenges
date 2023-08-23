5d6c3f7cb6ada90907dd24e05cbf29bc457845d7
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-23 11:05:17 +0100

    edited files

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,5 +1,19 @@
-function all (promises) {
-  return {}
+function all(promises) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let completedPromises = 0;
+
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(value => {
+        results[i] = value;
+        completedPromises++;
+
+        if (completedPromises === promises.length) {
+          resolve(results);
+        }
+      }).catch(reject);
+    }
+  });
 }
 
-module.exports = all
+module.exports = all;
