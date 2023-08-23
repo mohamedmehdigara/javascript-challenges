c5cd5d0dffd34f0a5fefa0b3a676a96bdffe5074
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-23 09:51:55 +0100

    edited files

--- object-diff/object-diff.js
+++ object-diff/object-diff.js
@@ -1,5 +1,21 @@
 const diff = (newCode, oldCode) => {
-  return []
+  const result = [];
+
+  for (const key in oldCode) {
+    if (!(key in newCode)) {
+      result.push(['-', key, oldCode[key]]);
+    }
+  }
+
+  for (const key in newCode) {
+    if (!(key in oldCode)) {
+      result.push(['+', key, newCode[key]]);
+    } else if (newCode[key] !== oldCode[key]) {
+      result.push([' ', key, newCode[key]]);
+    }
+  }
+
+  return result;
 }
 
-module.exports = diff
+module.exports = diff;
