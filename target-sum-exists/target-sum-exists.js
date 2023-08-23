df3f70b929d523bd46c1fb832b1aa00a2641ca13
mohamedmehdigara <garamehdi57@yahoo.com>
2023-08-23 14:21:19 +0100

    edited files

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,12 +1,29 @@
-const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
+function targetSumExistsInTwoNumbers(arr, target) {
+  const numSet = new Set();
+
+  for (const num of arr) {
+    const complement = target - num;
+    if (numSet.has(complement)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+
+  return false;
 }
 
-const targetSumExistsInThreeNumbers = (list, target) => {
-  return false
+function targetSumExistsInThreeNumbers(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    const remainingSum = target - arr[i];
+    if (targetSumExistsInTwoNumbers(arr.slice(0, i).concat(arr.slice(i + 1)), remainingSum)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 module.exports = {
   targetSumExistsInTwoNumbers,
   targetSumExistsInThreeNumbers
-}
+};
